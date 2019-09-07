3fe59bf35a80577380fa7ff0955a39b3c25ec30a
RaneemAlsaati <44734860+RaneemAlsaati@users.noreply.github.com>
2019-09-07 12:12:44 +0200

    Create circle.js

---
+++ nodejs/week1/circle.js
@@ -0,0 +1,17 @@
+class circle {
+    constructor (redius) {
+        this.redius = redius
+    }
+    getDiameter () {
+        return this.radius*2;
+    }
+    getCircumstance (){
+        return this.circumstance = 2*3.14*radius;
+    }
+    getArea () {
+        return this.area = 3.14*Math.pow (this.radius*2);
+    }
+ };
+ const circle = new Circle(10);
+const diameter = circle.getDiameter(); // 20
+    
